7df6b25f708736068e4736243060c16a3b6255fc
Michael <michaeltandyo@gmail.com>
2023-02-16 19:37:12 -0700

    changed everything from jsx -> js

--- my-app/src/Components/App.js
+++ my-app/src/Components/App.js
@@ -1,19 +1,13 @@
 import Header from './Header';
 import Home from "./Home";
-
 import Browse from "./Browse"
 import css from "../App.css"
-import {BrowserRouter, Route, Routes} from "react-router-dom"
-
 function App() {
- 
   return (
-    
     <div className="App">
       <Header/>
-      <Home/>
-      
-     
+      {/* <Home/> */}
+      <Browse/>
     </div>
   );
 }
--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -1,9 +1,7 @@
-import { useEffect, useState } from 'react';
-import useDatabase from '../Database/Database';
+
 export default function Home(){
-     
     return(
-       
+        
         <div className="bg-cover bg-center" style={{ backgroundImage: "url(https://via.placeholder.com/1920x1080)", height: "100vh" }}>
             <div className="container mx-auto py-60">
                  <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg">
