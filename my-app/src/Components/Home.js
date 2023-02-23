0013890b3530518219fd940b11da74af6987a172
tsangkafu <tsangkafu@ymail.com>
2023-02-22 19:30:15 -0700

    refine fav

--- my-app/src/Components/Home.js
+++ my-app/src/Components/Home.js
@@ -4,7 +4,7 @@ import Browse from "./Browse"
 export default function Home(){
     return(
         
-        <div className="bg-cover bg-center" style={{ backgroundImage: "url(https://via.placeholder.com/1920x1080)", height: "100vh" }}>
+        <div className="bg-cover bg-center">
             <div className="container mx-auto py-60">
                  <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg">
                  <input className="my-4 w-full p-4 rounded-lg " type="text" placeholder="Search Movies..." />
@@ -14,7 +14,6 @@ export default function Home(){
                     <button className= "w-60 p-4 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 ">
                     <Link to="/browse">Browse All Movies</Link>
                     </button>
-                    
                  </div>
             </div>
             
