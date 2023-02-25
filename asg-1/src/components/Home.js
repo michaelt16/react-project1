b4c348a2475f1ab08a1b58097e2ba69a46d997e9
tsangkafu <tsangkafu@ymail.com>
2023-02-24 19:22:25 -0700

    added background image

--- asg-1/src/components/Home.js
+++ asg-1/src/components/Home.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
 import Favorite from "./Favorite";
+import background from "../img/background.jpg";
 
 export default function Home(props){
 
     return(
-        <div className="grid grid-cols-5 bg-cover bg-center" style={{ backgroundImage: "url(https://via.placeholder.com/1920x1080)", height: "100vh" }}>
+        <div className="grid grid-cols-5 bg-cover bg-center" style={{ backgroundImage: `url(${background})`, height: "100vh" }}>
             <div className={`py-60 col-span-4`}>
                  <div className="max-w-lg mx-auto p-8 bg-white rounded-lg shadow-lg">
                     <input className="my-4 w-full p-4 rounded-lg " type="text" placeholder="Search Movies..." />
