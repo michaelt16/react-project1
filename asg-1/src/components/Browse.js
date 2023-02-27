69f4bc922eb4417b8a8c6056458c313fb1ce949e
Michael <mtand920@mtroyal.ca>
2023-02-27 12:32:07 -0800

    finished filters and sort

--- asg-1/src/components/Browse.js
+++ asg-1/src/components/Browse.js
@@ -1,18 +1,34 @@
 import Favorite from "./Favorite";
 import Filter from "./Filter"
 import MovieList from "./MovieList";
+import { useState } from "react";
 
 export default function Browse(props){
 
+    const [initialMovies]= useState(props.movies)
+
+    const handleChange = (newVal)=>{ 
+        props.setMovies(newVal)
+        console.log("updatesd",newVal)
+    }
     return(
         <div className="grid grid-cols-5 h-screen">
-            <Filter/>
+            {console.log("initialMovies",initialMovies)}
+
+             <Filter movies= {props.movies} 
+            genreList ={props.genreList} 
+            initialMovies = {initialMovies}
+            setMovies = {props.setMovies}
+            onSubmit={handleChange}
+            />
             <MovieList
                 movies={props.movies}
                 setMovies={props.setMovies}
                 favoriteVisible={props.favoriteVisible}
                 setFavorite={props.setFavorite}
                 handleImageError={props.handleImageError}
+                //onClick={handleChange}
+                initialMovies={initialMovies}
                 />
 
             {props.favoriteVisible && 
